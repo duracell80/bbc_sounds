03722a97f85e429a411c8ff3be821dfa55084915
Lee Jordan <duracell80@users.noreply.github.com>
2020-06-28 18:04:48 -0500

    Create play_latest.sh

---
+++ play_latest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+mkdir -p downloads/BBC/"$1"/"$2"
+cd downloads/BBC/"$1/$2"
+get_iplayer --available-since 72 --listformat "<pid>" --type=radio --channel="$1" "$2" | tail -2 | head -n 1 | xargs -I {} get_iplayer --force --overwrite --pid="{}"
+
+sudo mkdir -p /var/lib/mpd/music/ROD
+sudo cp -rf ../../../../downloads/* /var/lib/mpd/music/ROD
+rm -rf downloads/BBC/"$1/"
+
+mpc update
+mpc clear
+mpc insert "ROD/BBC/$1/$2"
+mpc play

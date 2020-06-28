20ab36f36fb249fd2f64aa14acbd9fbea5198e23
Lee Jordan <duracell80@users.noreply.github.com>
2020-06-28 17:54:51 -0500

    Update get_latest.sh

--- get_latest.sh
+++ get_latest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p downloads/BBC/"$1"/"$2"
 cd downloads/BBC/"$1/$2"
-get_iplayer --available-since 24 --listformat "<pid>" --type=radio --channel="$1" "$2" | tail -2 | head -n 1 | xargs -I {} get_iplayer --force --overwite --pid="{}"
+get_iplayer --available-since 72 --listformat "<pid>" --type=radio --channel="$1" "$2" | tail -2 | head -n 1 | xargs -I {} get_iplayer --force --overwite --pid="{}"
 
 sudo mkdir -p /var/lib/mpd/music/ROD
 sudo mv -f ../../../../downloads/* /var/lib/mpd/music/ROD

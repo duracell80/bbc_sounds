0b60618498047010f84327f6cac62405e77375ec
Lee Jordan <duracell80@users.noreply.github.com>
2020-06-28 18:03:36 -0500

    Update get_latest.sh

--- get_latest.sh
+++ get_latest.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 mkdir -p downloads/BBC/"$1"/"$2"
 cd downloads/BBC/"$1/$2"
-get_iplayer --available-since 72 --listformat "<pid>" --type=radio --channel="$1" "$2" | tail -2 | head -n 1 | xargs -I {} get_iplayer --force --overwite --pid="{}"
+get_iplayer --available-since 72 --listformat "<pid>" --type=radio --channel="$1" "$2" | tail -2 | head -n 1 | xargs -I {} get_iplayer --force --overwrite --pid="{}"
 
 sudo mkdir -p /var/lib/mpd/music/ROD
-sudo mv -f ../../../../downloads/* /var/lib/mpd/music/ROD
+sudo cp -rf ../../../../downloads/* /var/lib/mpd/music/ROD
+rm -rf downloads/BBC/"$1/"
+
 mpc update
 mpc clear
 mpc insert "ROD/BBC/$1/$2"
